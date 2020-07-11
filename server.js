685cdce33b1c8e91c6a94e01aba11b6d29e3fcde
ibrahim albimani <abukhalil2b@gmail.com>
2020-07-11 16:26:39 +0400

    edit route

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bookRoute =require('./routes/bookRoute');
 const cateRoute =require('./routes/cateRoute');
 //routes
 // app.use('/',bookRoute);
-app.use('/book',bookRoute);
-app.use('/cate',cateRoute);
+app.use('/book/',bookRoute);
+app.use('/cate/',cateRoute);
 
 //port number
 const PORT = process.env.PORT || 5000;
